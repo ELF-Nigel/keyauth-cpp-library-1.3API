a940b9ecbec8ea2ee554cd3eb4b50c793879d995
ELF-Nigel <ELF-Nigel@users.noreply.github.com>
2026-03-01 11:09:10 -0500

    Fix libcurl read callback usage

--- auth.cpp
+++ auth.cpp
@@ -2610,9 +2610,10 @@ std::string KeyAuth::api::req(std::string data, const std::string& url) {
     curl_easy_setopt(curl, CURLOPT_CERTINFO, 1L);
     curl_easy_setopt(curl, CURLOPT_NOPROXY, XorStr("keyauth.win").c_str());
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, nullptr);
     curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
     curl_easy_setopt(curl, CURLOPT_READDATA, &post);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, static_cast<long>(post.len));
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, static_cast<curl_off_t>(post.len));
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &to_return);
     curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
